984bfeedb328f02084c412c0c3e22b1a050a932a
gradampl <gr_adam@o2.pl>
2020-05-09 01:10:42 +0200

    Authenticate moved to AppController

--- Blog/app/controllers/application_controller.rb
+++ Blog/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+   @owner_name = ENV['OWNER_NAME']
+   @owner_password = ENV['OWNER_PASSWORD']
+   http_basic_authenticate_with name: @owner_name, password: @owner_password, except: [:index, :show, :create]
 end
